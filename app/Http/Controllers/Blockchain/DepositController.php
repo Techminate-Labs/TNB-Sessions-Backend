c41a240810e023734e6dba7799e4f650aeac5bff
SazidAhmed <sazidahmed.official@gmail.com>
2022-01-02 14:18:57 +0600

    integreting blockchain

---
+++ app/Http/Controllers/Blockchain/DepositController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Blockchain;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DepositController extends Controller
+{
+    //
+}
