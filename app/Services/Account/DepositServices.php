2d7d97e8e7679d2cb476ee860e7e7b27f65bf493
SazidAhmed <sazidahmed.official@gmail.com>
2022-01-07 17:40:31 +0600

    working on account creation

--- app/Services/Account/DepositServices.php
+++ app/Services/Account/DepositServices.php
@@ -9,6 +9,7 @@ use App\Services\Validation\Account\DepositValidation;
 
 //Models
 use App\Models\Deposit;
+use App\Models\Tempregister;
 
 class DepositServices extends BaseServices{
 
@@ -16,6 +17,7 @@ class DepositServices extends BaseServices{
      * http://54.183.16.194/bank_transactions?recipient=a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0&ordering=-block__created_date
      * http://54.183.16.194/bank_transactions?recipient=8c44cb32b7b0394fe7c6a8c1778d19d095063249b734b226b28d9fb2115dbc74&ordering=-block__created_date
      * http://54.183.16.194/confirmation_blocks?block=aa5c09a7-c573-4dd1-b06b-b123eb5880ff
+     * http://54.183.16.194/confirmation_blocks?block=&block__signature=a44d171d9f0ba0f6d0c0c489b9a17d24dd38734a4428fdf85ea08e1ca821086dae6601d20c3a0bcfc94e73b3f77092026d179391752702dd76adf38c50b8cb06
      */
     private  $depositModel = Deposit::class;
 
@@ -81,9 +83,13 @@ class DepositServices extends BaseServices{
         $deposit->save();
 
         //check register model
-
+        $registation = Tempregister::where('account_number',$deposit->sender)->where('verification_code', $deposit->memp)->first();
         //create account
-
+        if($registation) {
+           return 'create new account';
+        }else{
+            return 'update account information';
+        }
         //update account
     }
 
