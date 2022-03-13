736a375a1c3bba318a69f2c148b983868e59ab6b
SazidAhmed <sazidahmed.official@gmail.com>
2022-03-13 23:13:34 +0600

    working on withdraw system

---
+++ app/Http/Controllers/Account/WithdrawController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Service
+use App\Services\Account\WithdrawServices;
+
+class WithdrawController extends Controller
+{
+    private $withdrawServices;
+
+    public function __construct(WithdrawServices $withdrawServices){
+        $this->services = $withdrawServices;
+    }
+
+    public function withdraw(){
+        return "ok";
+    }
+
+    public function checkConfirmations(){
+        return "ok";
+    }
+}
