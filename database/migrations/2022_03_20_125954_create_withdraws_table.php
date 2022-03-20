b7696dea9213fef489ce95e8799430900054436b
SazidAhmed <sazidahmed.official@gmail.com>
2022-03-20 19:24:34 +0600

    started working on withdraw tracker

---
+++ database/migrations/2022_03_20_125954_create_withdraws_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWithdrawsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('withdraws', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('account_number')->nullable();
+            $table->BigInteger('account_balance');
+            $table->BigInteger('withdrawable_balance');
+            $table->BigInteger('withdraw_amount');
+            $table->BigInteger('status');
+            $table->BigInteger('new_balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('withdraws');
+    }
+}
